0d3d91dccf0475e367b18f256cea55b88b0ee71f
matt <05wsnmli@gmail.com>
2022-06-24 22:29:17 +0100

    renamed install script

--- ubuntu_install_programs.sh
+++ ubuntu_install_programs.sh
@@ -2,4 +2,6 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install cmus opam gcc gfortran texlive-full vim \
 clisp chezscheme golang-go openmpi-bin libopenmpi-dev \
-youtube-dl vlc
+youtube-dl vlc zathura stterm sl lolcat yakuake \
+lollypop clementine transmission-gtk calibre ranger
+
