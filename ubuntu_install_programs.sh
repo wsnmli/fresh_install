c31e2338f32b99c4659f4b685e17f7c21459b39e
matt <05wsnmli@gmail.com>
2022-06-26 15:23:02 +0100

    updated ubuntu install sctipt

--- ubuntu_install_programs.sh
+++ ubuntu_install_programs.sh
@@ -3,5 +3,6 @@ sudo apt update && sudo apt upgrade
 sudo apt install cmus opam gcc gfortran texlive-full vim \
 clisp chezscheme golang-go openmpi-bin libopenmpi-dev \
 youtube-dl vlc zathura stterm sl lolcat yakuake \
-lollypop clementine transmission-gtk calibre ranger
+lollypop clementine transmission-gtk calibre ranger \
+libsdl2-ttf-dev libsdl2-dev
 
