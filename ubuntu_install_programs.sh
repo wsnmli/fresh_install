47f6756a793a2fdd9166c531f16cc06684706f65
wsnmli <05wsnmli@gmail.com>
2022-10-05 16:10:42 +0100

    Update ubuntu_install_programs.sh
    added blender

--- ubuntu_install_programs.sh
+++ ubuntu_install_programs.sh
@@ -4,5 +4,5 @@ sudo apt install cmus opam gcc gfortran texlive-full vim \
 clisp chezscheme golang-go openmpi-bin libopenmpi-dev \
 youtube-dl vlc zathura stterm sl lolcat yakuake \
 lollypop clementine transmission-gtk calibre ranger \
-libsdl2-ttf-dev libsdl2-dev
+libsdl2-ttf-dev libsdl2-dev blender
 
