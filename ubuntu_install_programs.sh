c1aad855a9683173714151191baca74c2da7fe9d
wsnmli <05wsnmli@gmail.com>
2022-10-06 20:54:23 +0100

    added programs needed to make opengl programs

--- ubuntu_install_programs.sh
+++ ubuntu_install_programs.sh
@@ -4,5 +4,6 @@ sudo apt install cmus opam gcc gfortran texlive-full vim \
 clisp chezscheme golang-go openmpi-bin libopenmpi-dev \
 youtube-dl vlc zathura stterm sl lolcat yakuake \
 lollypop clementine transmission-gtk calibre ranger \
-libsdl2-ttf-dev libsdl2-dev blender
+libsdl2-ttf-dev libsdl2-dev blender freeglut3 freeglut3-dev \
+gimp krita
 
