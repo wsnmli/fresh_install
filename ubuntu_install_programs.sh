7c222a51cfc6a06cc0ebbfa53fe8b5332a1d8a83
wsnmli <05wsnmli@gmail.com>
2023-06-24 13:58:17 +0100

    Update ubuntu_install_programs.sh

--- ubuntu_install_programs.sh
+++ ubuntu_install_programs.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt upgrade
+sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install cmus opam gcc gfortran texlive-full vim \
 clisp chezscheme golang-go openmpi-bin libopenmpi-dev \
@@ -6,5 +6,6 @@ youtube-dl vlc zathura stterm sl lolcat yakuake \
 lollypop clementine transmission-gtk calibre ranger \
 libsdl2-ttf-dev libsdl2-dev blender freeglut3 freeglut3-dev \
 gimp krita opengl-4-man-doc tmux fmit cmake ghc \
-python3-pip libsdl2-image-dev r-base-core wine krita
+python3-pip libsdl2-image-dev r-base-core wine krita \
+screenfetch gcc-multilib g++-multilib -y
 
